a94eab1c787bd3e111367361e98186df3d765d95
Omar Qureshi <omaraliqureshi@googlemail.com>
2011-07-06 13:34:29 +0100

    First attempt at a mongodb package

---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+class mongodb {
+  case $architecture {
+    x86_64: {
+      yumrepo {"10gen":
+        baseurl => "http://downloads-distro.mongodb.org/repo/redhat/os/x86_64",
+        gpgcheck => 0,
+        descr => "10gen Repository",
+        enabled => 1,
+      }
+    }
+    i686: {
+      yumrepo {"10gen":
+        baseurl => "http://downloads-distro.mongodb.org/repo/redhat/os/i686",
+        gpgcheck => 0,
+        descr => "10gen Repository",
+        enabled => 1,
+      }
+    }
+  }
+
+  package {"mongo-10gen-server": ensure => installed, require => Yumrepo["10gen"]}
+  package {"mongo-10gen": ensure => installed, require => Yumrepo["10gen"]}
+}
